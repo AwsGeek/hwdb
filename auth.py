8a58f7948a2f19f9574930b3342c26b933e3b0ea
Jerry Hargrove <jerryhargrove@gmail.com>
2023-12-24 20:10:19 +0000

    Use SQLAlchemy

--- auth.py
+++ auth.py
@@ -1,10 +1,7 @@
-# Python standard libraries
-import json
 import os
-import sqlite3
+import json
+from flask import Blueprint, render_template, Flask, redirect, request, url_for
 
-# Third-party libraries
-from flask import Flask, redirect, request, url_for
 from flask_login import (
     LoginManager,
     current_user,
@@ -12,12 +9,13 @@ from flask_login import (
     login_user,
     logout_user,
 )
+from . import db
+from .models import User
+
 from oauthlib.oauth2 import WebApplicationClient
 import requests
 
-# Internal imports
-from db import init_db_command
-from user import User
+auth = Blueprint('auth', __name__)
 
 # Configuration
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
@@ -26,47 +24,11 @@ GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
 
-# Flask app setup
-app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
-
-# User session management setup
-# https://flask-login.readthedocs.io/en/latest
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-# Naive database setup
-try:
-    init_db_command()
-except sqlite3.OperationalError:
-    # Assume it's already been created
-    pass
-
 # OAuth 2 client setup
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
-# Flask-Login helper to retrieve a user from our db
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
 
-
-@app.route("/")
-def index():
-    if current_user.is_authenticated:
-        return (
-            "<p>Hello, {}! You're logged in! Email: {}</p>"
-            "<div><p>Google Profile Picture:</p>"
-            '<img src="{}" alt="Google profile pic"></img></div>'
-            '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.name, current_user.email, current_user.profile_pic
-            )
-        )
-    else:
-        return '<a class="button" href="/login">Google Login</a>'
-
-
-@app.route("/login")
+@auth.route('/login')
 def login():
     # Find out what URL to hit for Google login
     google_provider_cfg = get_google_provider_cfg()
@@ -82,7 +44,7 @@ def login():
     return redirect(request_uri)
 
 
-@app.route("/login/callback")
+@auth.route("/login/callback")
 def callback():
     # Get authorization code Google sent back to you
     code = request.args.get("code")
@@ -122,37 +84,33 @@ def callback():
     if userinfo_response.json().get("email_verified"):
         unique_id = userinfo_response.json()["sub"]
         users_email = userinfo_response.json()["email"]
-        picture = userinfo_response.json()["picture"]
+        users_picture = userinfo_response.json()["picture"]
         users_name = userinfo_response.json()["given_name"]
     else:
         return "User email not available or not verified by Google.", 400
 
-    # Create a user in our db with the information provided
-    # by Google
-    user = User(
-        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
-    )
+    user = User.query.filter_by(email=users_email).first() # if this returns a user, then the email already exists in database
+    if not user: # Doesn't exist? Add to database
+        
+        user = User(email=users_email, name=users_name, profile_pic=users_picture)
 
-    # Doesn't exist? Add to database
-    if not User.get(unique_id):
-        User.create(unique_id, users_name, users_email, picture)
+        db.session.add(user)
+        db.session.commit()
+        db.session.refresh(user)
 
     # Begin user session by logging the user in
     login_user(user)
 
     # Send user back to homepage
-    return redirect(url_for("index"))
+    return redirect(url_for("main.index"))
 
 
-@app.route("/logout")
+@auth.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("index"))
+    return redirect(url_for("main.index"))
 
 
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
-
-if __name__ == "__main__":
-    app.run(ssl_context="adhoc")
